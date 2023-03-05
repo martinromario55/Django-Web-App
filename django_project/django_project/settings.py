33c199c9818b12422c7d091cb3ad80384c4a5aed
martinromario55 <martinromario55@gmail.com>
2023-03-05 12:51:32 +0300

    Add Procfile, runtime file

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yzi!2tml2x*)l*fh-xx^4*@b)9+u4xg1-z+$n^g2h-z9e72=^q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['martinromario-blog-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
